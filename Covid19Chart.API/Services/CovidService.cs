aafce9e179fd61464dc99b42b3a71b11346c9191
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-02-05 19:00:25 +0300

    CovidService oluşturuldu.

---
+++ Covid19Chart.API/Services/CovidService.cs
@@ -0,0 +1,32 @@
+﻿using Covid19Chart.API.Hubs;
+using Covid19Chart.API.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Covid19Chart.API.Services
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IHubContext<CovidHub> _hubContext;
+
+        public CovidService(AppDbContext dbContext, IHubContext<CovidHub> hubContext)
+        {
+            _dbContext = dbContext;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Covid> GetList()
+        {
+            return _dbContext.Covids.AsQueryable();
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+            await _dbContext.Covids.AddAsync(covid);
+            await _dbContext.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList","data");
+        }
+
+
+    }
+}
